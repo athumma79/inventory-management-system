34015d4c6ae510d95b2681dd672bdbfe6ea6c80a
athumma79 <35754386+athumma79@users.noreply.github.com>
2021-10-27 17:53:08 -0400

    added endpoints

--- api/app.js
+++ api/app.js
@@ -2,8 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.json("hello world")
+app.get('/cars', (req, res) => {
+    
+})
+
+app.post('/cars', (req, res) => {
+    
+})
+
+app.put('/cars/:vin', (req, res) => {
+    
+})
+
+app.delete('/cars/:vin', (req, res) => {
+    
+})
+
+app.get('/cars/:vin/image', (req, res) => {
+    
+})
+
+app.post('/cars/:vin/image', (req, res) => {
+    
 })
 
 app.listen(port, () => {
