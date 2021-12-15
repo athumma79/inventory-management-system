f357395891cb101fcaf1984ae9abf8a325787804
Apoorva Thumma <apoorva.thumma79@gmail.com>
2021-12-15 03:48:13 -0500

    integrated ui with aws api

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api: "https://6dis2jltik.execute-api.us-east-1.amazonaws.com/default"
 };
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: "http://localhost:3000"
+  api: "https://6dis2jltik.execute-api.us-east-1.amazonaws.com/default"
 };
 
 /*
